eaf63ba6fffb14d3612880109b252e2fc356c4d8
Daniel Green <KasumiL5x@gmail.com>
2016-08-08 08:10:13 +0100

    DrawRect now has a color input.  In addition, added a fullscreen BG to the kana popup alongside the banner.

--- KanaSwap/Assets/scripts/DrawRect.cs
+++ KanaSwap/Assets/scripts/DrawRect.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class DrawRect : MonoBehaviour {
+	public Color PixelColor;
 	Sprite sprite_ = null;
 
 	void Start () {
 		Texture2D tex = new Texture2D(1, 1, TextureFormat.ARGB32, false);
-		tex.SetPixel(0, 0, new Color(0.0f, 0.0f, 0.0f, 0.75f));
+		tex.SetPixel(0, 0, PixelColor);
 		tex.Apply();
 		sprite_ = Sprite.Create(tex, new Rect(0, 0, 1, 1), new Vector2(0.5f, 0.5f));
 

5ca40d1e7e4ad1ce56779408009f54785bf47608
Daniel Green <KasumiL5x@gmail.com>
2016-08-10 20:28:09 +0100

    Added buttons for shuffle and reset (that don't do anything yet) as well as a script to change button text colors when pressed/released.

---
+++ KanaSwap/Assets/scripts/DragDrop.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DragDrop : MonoBehaviour {
+	public Camera camera_;
+
+	void Update () {
+		bool pressed = Input.GetAxis("Fire1") > 0.0f;
+		if( pressed ) {
+			var hit = Physics2D.Raycast(camera_.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+			if( hit.collider != null ) {
+				Debug.Log ("Clicked: " + Utils.getFullPath(hit.transform.gameObject));
+			}
+		}
+	}
+}
---
+++ KanaSwap/Assets/scripts/buttons/ButtonColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ButtonColor : MonoBehaviour {
+	public UnityEngine.UI.Text text;
+	public Color PressedColor = new Color(0.74f, 0.68f, 0.63f, 1.0f);
+	public Color ReleasedColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+	public void onButtonClick() {
+		if( text != null ) {
+			text.color = PressedColor;
+		}
+	}
+
+	public void onButtonRelease() {
+		if( text != null ) {
+			text.color = ReleasedColor;
+		}
+	}
+}

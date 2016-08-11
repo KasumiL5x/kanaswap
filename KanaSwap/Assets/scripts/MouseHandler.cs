56858f774cc810b38f445e1c2de687827e159d91
Daniel Green <KasumiL5x@gmail.com>
2016-08-11 19:47:14 +0100

    Moved mouse handling into a separate class that calls events.  One is guaranteed to call and another followed handlers until one is succesful.  The bug of the overlaying collider not blocking other calls can be solved using this.

---
+++ KanaSwap/Assets/scripts/MouseHandler.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MouseHandler : MonoBehaviour {
+	public delegate void OnMouseDown( GameObject hit );
+	public delegate void OnMouseHold( GameObject hit );
+	public delegate void OnMouseUp( GameObject hit );
+
+	public delegate bool OnMouseDownB( GameObject hit );
+	public delegate bool OnMouseHoldB( GameObject hit );
+	public delegate bool OnMouseUpB( GameObject hit );
+
+	static List<OnMouseDown> MouseDown = new List<OnMouseDown>();
+	static List<OnMouseHold> MouseHold = new List<OnMouseHold>();
+	static List<OnMouseUp> MouseUp = new List<OnMouseUp>();
+
+	static List<OnMouseDownB> MouseDownB = new List<OnMouseDownB>();
+	static List<OnMouseHoldB> MouseHoldB = new List<OnMouseHoldB>();
+	static List<OnMouseUpB> MouseUpB = new List<OnMouseUpB>();
+
+	static GameObject ClickedObject = null;
+	public Camera camera_ = null;
+	bool wasMouseDown_ = false;
+
+	public static void addMouseDown( OnMouseDown cb ) {
+		MouseDown.Add(cb);
+	}
+
+	public static void addMouseDownB( OnMouseDownB cb ) {
+		MouseDownB.Add(cb);
+	}
+
+	public static void addMouseHold( OnMouseHold cb ) {
+		MouseHold.Add(cb);
+	}
+
+	public static void addMouseHoldB( OnMouseHoldB cb ) {
+		MouseHoldB.Add(cb);
+	}
+
+	public static void addMouseUp( OnMouseUp cb ) {
+		MouseUp.Add(cb);
+	}
+
+	public static void addMouseUpB( OnMouseUpB cb ) {
+		MouseUpB.Add(cb);
+	}
+
+	void Awake() {
+		if( null == camera_ ) {
+			Debug.Log("Camera is null.");
+		}
+	}
+
+	void Update () {
+		bool isMouseDown = Input.GetMouseButton(0);
+
+		if( isMouseDown && !wasMouseDown_ ) {
+			updateClickedObject();
+			foreach( var cb in MouseDown ) {
+				cb.Invoke(ClickedObject);
+			}
+			foreach( var cb in MouseDownB ) {
+				if( cb.Invoke(ClickedObject) ) {
+					break;
+				}
+			}
+		} else if( isMouseDown && wasMouseDown_ ) {
+			foreach( var cb in MouseHold ) {
+				cb.Invoke(ClickedObject);
+			}
+			foreach( var cb in MouseHoldB ) {
+				if( cb.Invoke(ClickedObject) ) {
+					break;
+				}
+			}
+		} else if( !isMouseDown && wasMouseDown_ ) {
+			updateClickedObject();
+			foreach( var cb in MouseUp ) {
+				cb.Invoke(ClickedObject);
+			}
+			foreach( var cb in MouseUpB ) {
+				if( cb.Invoke(ClickedObject) ) {
+					break;
+				}
+			}
+		}
+
+		wasMouseDown_ = isMouseDown;
+	}
+
+	void updateClickedObject() {
+		var hit = Physics2D.Raycast(camera_.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		ClickedObject = null == hit.transform ? null : hit.transform.gameObject;
+	}
+}
--- KanaSwap/Assets/scripts/kana_popup/CloseKanaPopup.cs
+++ KanaSwap/Assets/scripts/kana_popup/CloseKanaPopup.cs
@@ -4,12 +4,26 @@ using System.Collections;
 public class CloseKanaPopup : MonoBehaviour {
 	public KanaPopup popup_ = null;
 
-	void OnMouseUp() {
+	void Awake() {
+		MouseHandler.addMouseDownB(onMouseDown);
+	}
+
+	bool onMouseDown( GameObject hit ) {
+		if( null == hit ) {
+			return false;
+		}
+
+		if( gameObject != hit ) {
+			return false;
+		}
+
 		if( null == popup_ ) {
 			Debug.Log("Missing popup.");
-			return;
+			return false;
 		}
 
 		popup_.deactivate();
+
+		return true;
 	}
 }

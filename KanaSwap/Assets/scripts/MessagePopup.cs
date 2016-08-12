80cdf2615fd59c77de3deaaeadfbbb89c0361b5f
Daniel Green <KasumiL5x@gmail.com>
2016-08-12 10:10:17 +0100

    Added message popup beginnings and possibly the best nullable checking class ever.

---
+++ KanaSwap/Assets/scripts/MessagePopup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessagePopup : MonoBehaviour {
+	Fader fader_;
+	TextMesh textMesh_;
+	float timer_ = 0.0f;
+	bool isAlive_ = false;
+
+	void Awake() {
+		fader_ = gameObject.GetComponent<Fader>();
+		if( null == fader_ ) {
+			Debug.Log("Missing Fader.");
+		}
+
+		textMesh_ = transform.Find("text").With(x => x.GetComponent<TextMesh>());
+		if( null == textMesh_ ) {
+			Debug.Log("Missing TextMesh.");
+		}
+	}
+
+	void Update() {
+		if( !isAlive_ ) {
+			return;
+		}
+
+		timer_ -= Time.deltaTime;
+		if( timer_ <= 0.0f ) {
+			fader_.fadeOut();
+			isAlive_ = false;
+		}
+	}
+
+	public void show( string text, float time=1.0f ) {
+		gameObject.SetActive(true);
+		fader_.fadeIn();
+		timer_ = time;
+		isAlive_ = true;
+	}
+}

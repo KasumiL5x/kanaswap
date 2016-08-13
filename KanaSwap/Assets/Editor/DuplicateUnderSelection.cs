36ce37315621036daea8d677bbb73e8d7009a461
Daniel Green <KasumiL5x@gmail.com>
2016-08-13 12:16:03 +0100

    Added a new script to duplicate a given object as a child of selected objects.

---
+++ KanaSwap/Assets/Editor/DuplicateUnderSelection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class DuplicateUnderSelection : EditorWindow {
+	GameObject objToDupe_ = null;
+
+	[MenuItem("Selection/Duplicate Under Selection")]
+	static void Init() {
+		DuplicateUnderSelection window = (DuplicateUnderSelection)EditorWindow.GetWindow(typeof(DuplicateUnderSelection), false, "Duplicate Under Selection");
+		window.Show();
+	}
+
+	void doWork() {
+		foreach( GameObject obj in Selection.GetFiltered(typeof(GameObject), SelectionMode.ExcludePrefab) ) {
+			var dupe = (GameObject)GameObject.Instantiate(objToDupe_);
+			dupe.name = objToDupe_.name;
+			dupe.transform.SetParent(obj.transform);
+			dupe.transform.localPosition = objToDupe_.transform.localPosition;
+			dupe.transform.localRotation = objToDupe_.transform.localRotation;
+		}
+	}
+
+	void OnGUI() {
+		objToDupe_ = (GameObject)EditorGUI.ObjectField (new Rect (10, 10, position.width - 20, 20), "Object to Duplicate", objToDupe_, typeof(GameObject), true);
+		GUI.enabled = objToDupe_ != null;
+		if( GUI.Button(new Rect (10, 40, position.width - 20, 20), "Duplicate under Selected") ) {
+			doWork();
+		}
+		GUI.enabled = true;
+	}
+}

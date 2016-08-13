1e4c395f57ff90a2fffda080773b8a1f5e5a117e
Daniel Green <KasumiL5x@gmail.com>
2016-08-13 12:26:34 +0100

    Housework on KanaHandler.

--- KanaSwap/Assets/scripts/KanaHandler.cs
+++ KanaSwap/Assets/scripts/KanaHandler.cs
@@ -82,7 +82,7 @@ public class KanaHandler : MonoBehaviour {
 				return false;
 			}
 
-			if( kana.CurrentType != kana.ActualType ) {
+			if( !kana.isCorrect() ) {
 				return false;
 			}
 		}
@@ -91,13 +91,7 @@ public class KanaHandler : MonoBehaviour {
 
 	public void updateAllText() {
 		foreach( Transform xform in transform ) {
-			var kana = xform.gameObject.GetComponent<Kana>();
-			if( null == kana ) {
-				Debug.Log(Utils.getFullPath(xform.gameObject) + " is missing a Kana.");
-				continue;
-			}
-
-			kana.forceUpdateText();
+			xform.gameObject.GetComponent<Kana>().Do(x => x.forceUpdateText());
 		}
 	}
 }

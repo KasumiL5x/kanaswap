5d3b607f502b1f3df6bfee6b7bf484a022c1a143
Daniel Green <KasumiL5x@gmail.com>
2016-08-13 23:10:54 +0100

    Kana colors now lerp.

--- KanaSwap/Assets/scripts/Kana.cs
+++ KanaSwap/Assets/scripts/Kana.cs
@@ -3,13 +3,44 @@
 public class Kana : MonoBehaviour {
 	public KanaType ActualType = KanaType.Invalid;
 	public KanaType CurrentType = KanaType.Invalid;
-	Color DefaultColor = new Color(0.8f, 0.75f, 0.71f, 1.0f); // #cdc0b4
+
+	Color DefaultColor   = new Color(0.8f, 0.75f, 0.71f, 1.0f); // #cdc0b4
 	Color IncorrectColor = new Color(0.6f, 0.56f, 0.50f, 1.0f); // #998e7f
-	Color HiliteColor = new Color(0.8f, 0.79f, 0.77f, 1.0f);// #ccc9c5
+	Color HiliteColor    = new Color(0.8f, 0.79f, 0.77f, 1.0f); // #ccc9c5
 	bool hilited_ = false;
 
+	float ColorLerpTime = 0.15f;
+	float timerColorLerp_ = 0.0f;
+	bool isLerpingColor_ = false;
+	Color lastColor_;
+	Color desiredColor_;
+	SpriteRenderer bgRenderer_;
+
+	public Kana() {
+		timerColorLerp_ = ColorLerpTime;
+		lastColor_ = desiredColor_ = DefaultColor;
+	}
+
 	void Awake() {
+		bgRenderer_ = transform.Find("bg").With(x => x.GetComponent<SpriteRenderer>());
+		bgRenderer_.Do(x => x.color = desiredColor_);
+
 		updateColor();
+	}
+
+	void Update() {
+		if( isLerpingColor_ ) {
+			timerColorLerp_ -= Time.deltaTime;
+
+			timerColorLerp_ = (timerColorLerp_ < 0.0f) ? 0.0f : timerColorLerp_;
+			Color newColor = Color.Lerp(lastColor_, desiredColor_, 1.0f - (timerColorLerp_ / ColorLerpTime));
+			bgRenderer_.color = newColor;
+
+			if( timerColorLerp_ <= 0.0f ) {
+				timerColorLerp_ = ColorLerpTime;
+				isLerpingColor_ = false;
+			}
+		}
 	}
 
 	public bool isCorrect() {
@@ -36,7 +67,9 @@ public class Kana : MonoBehaviour {
 	}
 
 	public void updateColor() {
-		Color color = hilited_ ? HiliteColor : (isCorrect() ? DefaultColor : IncorrectColor);
-		transform.Find("bg").With(x => x.GetComponent<SpriteRenderer>()).Do(x => x.color = color);
+		lastColor_ = bgRenderer_.color;
+		desiredColor_ = hilited_ ? HiliteColor : (isCorrect() ? DefaultColor : IncorrectColor);
+		timerColorLerp_ = ColorLerpTime;
+		isLerpingColor_ = true;
 	}
 }

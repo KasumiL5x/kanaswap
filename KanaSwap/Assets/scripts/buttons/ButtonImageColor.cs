abc63a6376a4a856a39ef15d8716b1ef75531334
Daniel Green <KasumiL5x@gmail.com>
2016-08-14 22:31:35 +0100

    Added shuffle and reset icons and buttons to replace the old UI buttons, and added a button script that colorizes the RawImage rather than Text.

---
+++ KanaSwap/Assets/scripts/buttons/ButtonImageColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ButtonImageColor : MonoBehaviour {
+	public UnityEngine.UI.RawImage image_;
+	public Color PressedColor = new Color(0.74f, 0.68f, 0.63f, 1.0f);
+	public Color ReleasedColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+	public void onButtonClick() {
+		if( image_ != null ) {
+			image_.color = PressedColor;
+		}
+	}
+
+	public void onButtonRelease() {
+		if( image_ != null ) {
+			image_.color = ReleasedColor;
+		}
+	}
+}

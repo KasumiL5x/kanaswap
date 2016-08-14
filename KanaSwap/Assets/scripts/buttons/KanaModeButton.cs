10671c4e7541d604a32b3d20e8faaa38c0644929
Daniel Green <KasumiL5x@gmail.com>
2016-08-14 13:48:36 +0100

    Replaced kana toggle with a square button. I plan to remove them all and replace them like this, with icons.

---
+++ KanaSwap/Assets/scripts/buttons/KanaModeButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class KanaModeButton : MonoBehaviour {
+	void Awake() {
+		updateText();
+	}
+
+	void updateText() {
+		var kanaModeText = KanaMode.Hiragana == Settings.KANA_MODE ? "ひ" : "カ";
+		transform.Find ("Text").With (x => x.GetComponent<UnityEngine.UI.Text> ()).Do (x => x.text = kanaModeText);
+	}
+
+	public void onClick() {
+		var changeKanaMode = GameObject.Find("Scripts").With(x => x.GetComponent<ChangeKanaMode>());
+		if( null == changeKanaMode ) {
+			Debug.Log("Could not find ChangeKanaMode on Scripts object.");
+			return;
+		}
+		changeKanaMode.change(KanaMode.Hiragana == Settings.KANA_MODE ? KanaMode.Katakana : KanaMode.Hiragana);
+
+		updateText();
+	}
+}

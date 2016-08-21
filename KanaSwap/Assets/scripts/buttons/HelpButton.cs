42dab2ff1b2a9ccd798753f2099ab44c61f0759c
Daniel Green <KasumiL5x@gmail.com>
2016-08-21 08:46:43 +0100

    Added mediocre help button... lol.

---
+++ KanaSwap/Assets/scripts/buttons/HelpButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class HelpButton : MonoBehaviour {
+	public MessagePopup message;
+
+	public void onClick() {
+		message.show ("Memorize kana by shuffling\nand reordering them! \n\n" +
+			"Created as a learning exercise by\nDaniel Green", 999.0f);
+	}
+}
